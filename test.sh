0d428786df0ad54eff0da57eeaf38d70b9247789
Aaron Clark <aaron.clark@circleci.com>
2023-05-15 15:39:59 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-adb shell am instrument -w com.circleci.samples.todoapp/androidx.test.runner.AndroidJUnitRunner
+test_results=$(adb shell am instrument -w com.circleci.samples.todoapp/androidx.test.runner.AndroidJUnitRunner)
+if grep -q "INSTRUMENTATION_STATUS_CODE: -1" $test_results; then
+    exit 1
+fi
