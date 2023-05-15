35e6361d4731cd28f4f4468841c9b7a59191e37a
Aaron Clark <aaron.clark@circleci.com>
2023-05-15 16:38:45 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,4 @@
 
 adb shell am instrument -w com.circleci.samples.todoapp/androidx.test.runner.AndroidJUnitRunner | grep "INSTRUMENTATION_FAILED"
 echo $?
-echo "determining exit code"
-echo "before results"
-cat output.log
-if cat test_results.txt | grep -q 'adb'; then echo "exit 1"; else echo "exit 0">
 
