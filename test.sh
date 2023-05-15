b5f57d9ce8c6b44e3cb8071966b161023f36f708
Aaron Clark <aaron.clark@circleci.com>
2023-05-15 15:42:22 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 test_results=$(adb shell am instrument -w com.circleci.samples.todoapp/androidx.test.runner.AndroidJUnitRunner)
-if grep -q "INSTRUMENTATION_STATUS_CODE: -1" $test_results; then
-    exit 1
+if grep -q "Starting test attempt 1" $test_results; then
+    echo "FAILURE!" && exit 1
 fi
