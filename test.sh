24bab3be214b471e2df29c9e176710fff54d9302
Aaron Clark <aaron.clark@circleci.com>
2023-05-15 16:13:43 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 test_results=$(adb shell am instrument -w com.circleci.samples.todoapp/androidx.test.runner.AndroidJUnitRunner)
+echo "determining exit code"
 if echo "$test_results" | grep -q 'attempt'; then echo "exit 1"; else echo "exit 0"; fi
+
