a1d9eaa1d1a6315ad93bd3d161f7bd0d7e8aba1f
Aaron Clark <aaron.clark@circleci.com>
2023-05-15 15:53:54 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-test_report=$(adb shell am instrument -w com.circleci.samples.todoapp/androidx.test.runner.AndroidJUnitRunner)
-exitcmd=$(if echo "$test_report" | grep -q 'attempt'; then echo "exit 1"; else echo "exit 0"; fi)
-$exitcmd
+adb shell am instrument -w com.circleci.samples.todoapp/androidx.test.runner.AndroidJUnitRunner > test.txt
+if cat test.txt | grep -q 'attempt'; then echo "exit 1"; else echo "exit 0"; fi
